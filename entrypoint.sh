4e60dfd98cb9799414ff4890110a6a3c5bef250e
amosgansweet <171889997+amosgansweet@users.noreply.github.com>
2025-10-29 11:59:22 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,21 +4,21 @@ set -e
 echo ">>> 启动 Cloudsbx 容器安装环境..."
 echo "当前时间：$(date)"
 
-# 若环境变量指定了协议端口或参数，导入
+# 若环境变量指定了协议端口或参数(环境变量用大写字母，原码是小写），导入
 export vlpt="${VLPT:-}"
-export vmpt="${VMPT:-}"
-export hypt="${HYPT:-}"
-export tupt="${TUPT:-}"
-export xhpt="${XHPT:-}"
-export vxpt="${VXPT:-}"
-export anpt="${ANPT:-}"
-export arpt="${ARPT:-}"
-export sspt="${SSPT:-}"
-export sopt="${SOPT:-}"
-export warp="${WARP:-}"
-export argo="${ARGO:-}"
-export agn="${ARGO_DOMAIN:-}"
-export agk="${ARGO_AUTH:-}"
+# export vmpt="${VMPT:-}"
+# export hypt="${HYPT:-}"
+# export tupt="${TUPT:-}"
+# export xhpt="${XHPT:-}"
+# export vxpt="${VXPT:-}"
+# export anpt="${ANPT:-}"
+# export arpt="${ARPT:-}"
+# export sspt="${SSPT:-}"
+# export sopt="${SOPT:-}"
+# export warp="${WARP:-}"
+# export argo="${ARGO:-}"
+# export agn="${ARGO_DOMAIN:-}"
+# export agk="${ARGO_AUTH:-}"
 
 # 启动主脚本
 bash /usr/local/bin/cloudsbx.sh
